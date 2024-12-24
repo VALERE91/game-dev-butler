d69d08b78de2aa1a7cc57dd7d5b22352dab07870
Valère Plantevin <valereplantevin@proton.me>
2024-12-24 17:53:41 -0500

    Fix js, svelte and TS files not recognized by Tailwind

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,7 @@
-<div>
-    <h1 class="text-3xl font-bold underline">Hello world!</h1>
+<script lang="ts">
+    import Greet from "$lib/Greet.svelte";
+</script>
+
+<div class="container h-full mx-auto flex justify-center items-center">
+    <h1 class="text-3xl underline">Hello World</h1>
 </div>
